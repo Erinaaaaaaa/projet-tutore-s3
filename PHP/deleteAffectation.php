7a87293305c8dc497fab9cde215efceb7821b0c1
NicolasCahard <45936372+NicolasCahard@users.noreply.github.com>
2019-12-20 11:21:26 +0100

    Add files via upload

---
+++ PHP/deleteAffectation.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'consultAffectation.php';
+require_once 'DB/DB.inc.php';
+
+
+// TODO: à transformer en fonction
+
+
+    $db = DB::getInstance();
+    if ($db == null) {
+        echo "Impossible de se connecter &agrave; la base de donneeacute;es !";
+    } else {
+        try {
+            $db->deleteAffectation($_GET['id'], $_GET['idModule']);
+            echo '<meta http-equiv="refresh" content="0;URL=../listAffectations.php">';
+
+        } catch (Exception $e) {
+            echo "ça MARCHE PAS: " . $e->getMessage();
+
+        }
+    }
+
+
+    $db->close();
