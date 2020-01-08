223cbd26759feec76b714a28e5841af753c6c4a9
NicolasCahard <45936372+NicolasCahard@users.noreply.github.com>
2020-01-08 16:13:50 +0100

    Update formAjouterEvenement.php

--- formAjouterEvenement.php
+++ formAjouterEvenement.php
@@ -41,6 +41,28 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 }
 
+//Colonne Role dans la table Type Event
+$user = getUtilisateur($_SESSION['login']);
+$roleUser = $user->getRole(); //1
+$length = strlen($roleUser); //2
+$tabRoles = array();
+for ($i=0; $i < $length; $i++) { 
+    array_push($tabRoles, $roleUser[$i]); //3
+}
+$type2 = array();
+$cpt = 0;
+foreach ($typeevenement as $event) {
+    $typeEvent = $event->getRoles();
+    while ($cpt < $length) { 
+        if( $tabRoles[$cpt] == $typeEvent ){
+            array_push($type2, $event);
+        }
+        $cpt++;
+    }
+    
+    $cpt=0;
+}
+
 $dateMax = new DateTime('06/30');
 $crea    = getUtilisateur($_SESSION['login'])->getCreeLe();
 
@@ -51,10 +73,12 @@ $dateMin = new DateTime();
 
 echo $tpl->render(array(
     "titre"=>"Inscription d'un évènement",
+    "tabTypes"=>$type2,
     "types"=>$typesSeance,
     "dateMin"=>$dateMin->format("Y-m-d"),
     "dateMax"=>$dateMax->format("Y-m-d"),
     "date"=>date("Y-m-d"),
-    "message"=>$message
+    "message"=>$message,
+    "user"=>$_SESSION['login'],
 
 ));
