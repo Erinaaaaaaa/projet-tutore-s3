0bd9b9dbacd9d5ac17a508cce417b08682fe6c94
NicolasCahard <45936372+NicolasCahard@users.noreply.github.com>
2020-01-08 16:21:15 +0100

    Update formAjouterEvenement.php

--- formAjouterEvenement.php
+++ formAjouterEvenement.php
@@ -42,6 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 }
 
 //Colonne Role dans la table Type Event
+$typeevenement = getTypesEvenement();
 $user = getUtilisateur($_SESSION['login']);
 $roleUser = $user->getRole(); //1
 $length = strlen($roleUser); //2
